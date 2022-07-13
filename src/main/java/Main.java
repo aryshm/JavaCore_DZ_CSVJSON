97701706d13e57f0b0d6eb5c79ccfa7332f35894
aryshm <arishensky@yandex.ru>
2022-07-13 16:57:36 +0400

    into modif

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,16 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -22,13 +25,35 @@ public class Main {
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json);
+        String fileName1 = "data.csv";
+        List<Employee> list1 = parseXML(fileName1);
+    }
+
+    private static List<Employee> parseXML(String fileName) {
+        List<Employee> staff = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse( new File(fileName));
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element element = (Element) node;
+                long id = Long.parseLong(element.getElementsByTagName("id").item(i).getNodeValue());
+                String firstName = element.getElementsByTagName("firstName").item(i).getNodeValue();
+                String lastName = element.getElementsByTagName("lastName").item(i).getNodeValue();
+                String country = element.getElementsByTagName("country").item(i).getNodeValue();
+                int age = Integer.parseInt(element.getElementsByTagName("age").item(i).getNodeValue());
+                staff.add(new Employee(id, firstName, lastName, country, age));
+            }
+        }
     }
 
     public static List<Employee>  parseCSV(String[] columnMapping, String fileName) {
         List<Employee> staff = new ArrayList<>();
         try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
-            ColumnPositionMappingStrategy<Employee> strategy =
-                    new ColumnPositionMappingStrategy<>();
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
             strategy.setType(Employee.class);
             strategy.setColumnMapping(columnMapping);
             CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
